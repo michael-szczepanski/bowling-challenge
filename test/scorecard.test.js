b40226e10d8d779a8dd36f026b74327810c92b68
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-05-24 16:26:40 +0100

    strikes and spares handling testing complete, all logic now implemneted

--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -16,37 +16,80 @@ describe('Scorecard', () => {
     expect(scorecard.calculateScore()).toEqual(133)
   })
 
-  test('calculateScore() counts scores for a gutter game', () => {
-    const scorecard = new Scorecard()
-    for (let i = 0; i < 9; i++) {
-      scorecard.addFrame(0, 0)
-    }
-    scorecard.addFrame(0, 0, 0)
-    expect(scorecard.calculateScore()).toEqual(0)
+  describe('calculateScore()', () => {
+    test('counts scores for a gutter game', () => {
+      const scorecard = new Scorecard()
+      for (let i = 0; i < 9; i++) {
+        scorecard.addFrame(0, 0)
+      }
+      scorecard.addFrame(0, 0, 0)
+      expect(scorecard.calculateScore()).toEqual(0)
+    })
+  
+    test('counts scores for a partial game', () => {
+      const scorecard = new Scorecard()
+      scorecard.addFrame(2, 5)
+      scorecard.addFrame(3, 5)
+      expect(scorecard.calculateScore()).toEqual(15)
+    })
+  
+    test('counts scores for a game with no strikes/spares, and gutter in second roll', () => {
+      const scorecard = new Scorecard()
+      for (let i = 0; i < 9; i++) {
+        scorecard.addFrame(9, 0)
+      }
+      scorecard.addFrame(9, 0, 0)
+      expect(scorecard.calculateScore()).toEqual(90)
+    })
+  
+    test('counts scores for a game with no strikes/spares, and mixed rolls', () => {
+      const scorecard = new Scorecard()
+      for (let i = 0; i < 9; i++) {
+        scorecard.addFrame(2, 3)
+      }
+      scorecard.addFrame(2, 3, 0)
+      expect(scorecard.calculateScore()).toEqual(50)
+    })
+  
+    test('counts scores for a perfect game', () => {
+      const scorecard = new Scorecard()
+      for (let i = 0; i < 9; i++) {
+        scorecard.addFrame(10, 0)
+      }
+      scorecard.addFrame(10, 10, 10)
+      expect(scorecard.calculateScore()).toEqual(300)
+    })
+
+    test('counts scores for a mixed spares game', () => {
+      const scorecard = new Scorecard()
+      scorecard.addFrame(0,10)
+      scorecard.addFrame(2,3)
+      expect(scorecard.calculateScore()).toEqual(17)
+    })
   })
 
-  test('calculateScore() counts scores for a partial game', () => {
-    const scorecard = new Scorecard()
-    scorecard.addFrame(2, 5)
-    scorecard.addFrame(3, 5)
-    expect(scorecard.calculateScore()).toEqual(15)
-  })
-
-  test('calculateScore() counts scores for a game with no strikes/spares, and gutter in second roll', () => {
-    const scorecard = new Scorecard()
-    for (let i = 0; i < 9; i++) {
-      scorecard.addFrame(9, 0)
-    }
-    scorecard.addFrame(9, 0, 0)
-    expect(scorecard.calculateScore()).toEqual(90)
-  })
-
-  test('calculateScore() counts scores for a game with no strikes/spares, and mixed rolls', () => {
-    const scorecard = new Scorecard()
-    for (let i = 0; i < 9; i++) {
-      scorecard.addFrame(2, 3)
-    }
-    scorecard.addFrame(2, 3, 0)
-    expect(scorecard.calculateScore()).toEqual(50)
+  describe('strike_extra_points()', () => {
+    test('counts scores for the next two rolls with no consecutive strike', () => {
+      const scorecard = new Scorecard()
+      scorecard.addFrame(10,0)
+      scorecard.addFrame(2,3)
+      expect(scorecard.strike_extra_points(0)).toEqual(5)
+    })
+  
+    test('counts scores if the next roll is a strike', () => {
+      const scorecard = new Scorecard()
+      scorecard.addFrame(10, 0)
+      scorecard.addFrame(10, 0)
+      scorecard.addFrame(2, 4)
+      expect(scorecard.strike_extra_points(0)).toEqual(12)
+    })
+  
+    test('counts scores if the next two rolls are both strikes', () => {
+      const scorecard = new Scorecard()
+      scorecard.addFrame(10, 0)
+      scorecard.addFrame(10, 0)
+      scorecard.addFrame(10, 0)
+      expect(scorecard.strike_extra_points(0)).toEqual(20)
+    })
   })
 })
